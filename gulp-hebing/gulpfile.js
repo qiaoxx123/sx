4b69d74836e8a0ad80f8ce89cbcb4f4b80fcf211
qiaoxx123 <qiao_1992_ceshi3@163.com>
2017-10-19 01:11:42 +0800

    xin-zeng

--- gulp-hebing/gulpfile.js
+++ gulp-hebing/gulpfile.js
@@ -1,11 +1,19 @@
-var gulp=require('gulp');
-var fileinclude  = require('gulp-file-include');
+//var gulp=require('gulp');
+//var fileinclude  = require('gulp-file-include');
+//
+//gulp.task('fileinclude', function() {
+//  gulp.src('src/**.html')
+//      .pipe(fileinclude({
+//        prefix: '@@',
+//        basepath: '@file'
+//      }))
+//  .pipe(gulp.dest('dist'));
+//});
+var gulp=require("gulp");
+var hec=require("gulp-file-include");
 
-gulp.task('fileinclude', function() {
-    gulp.src('src/**.html')
-        .pipe(fileinclude({
-          prefix: '@@',
-          basepath: '@file'
-        }))
-    .pipe(gulp.dest('dist'));
-});
+gulp.task("hecheng",function(){
+	gulp.src('i/**.html')
+	.pipe(hec())
+	.pipe(gulp.dest('a'));
+})
